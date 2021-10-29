111bde79b23df3188fbb0851428a3edc64d91176
facs01 <francsilva03@gmail.com>
2021-10-29 10:17:02 +0200

    3

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ const config = {
   PORT: process.env.PORT || 3000,
   MONGODB_HOST: process.env.MONGODB_HOST || "localhost",
   MONGODB_DATABASE: process.env.MONGODB_DB || "notes-app",
-  MONGODB_URI: `mongodb://${process.env.MONGODB_HOST || "localhost"}/${
+  MONGODB_URI: process.env.MONGODB_URI || `mongodb://${process.env.MONGODB_HOST || "localhost"}/${
     process.env.MONGODB_DATABASE || "notes-app"
   }`,
 };
